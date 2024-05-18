b7d32105f05056962493d86e4bd1cf7ec97dd37b
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-05-19 01:01:17 +0400

    Topic class removed

--- src/main/java/org/intellij/plugins/markdown/settings/MarkdownSettingsChangedListener.java
+++ src/main/java/org/intellij/plugins/markdown/settings/MarkdownSettingsChangedListener.java
@@ -2,7 +2,6 @@ package org.intellij.plugins.markdown.settings;
 
 import consulo.annotation.component.ComponentScope;
 import consulo.annotation.component.TopicAPI;
-import consulo.component.messagebus.Topic;
 import org.jetbrains.annotations.NotNull;
 
 @TopicAPI(ComponentScope.APPLICATION)
