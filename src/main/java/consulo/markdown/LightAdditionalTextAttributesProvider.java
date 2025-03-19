bcc8ef71a21a28a9ce1c0af84093cfe6b79de654
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-03-19 11:35:31 +0400

    replace extending color scheme via new api

--- src/main/java/consulo/markdown/LightAdditionalTextAttributesProvider.java
+++ src/main/java/consulo/markdown/LightAdditionalTextAttributesProvider.java
@@ -1,26 +1,34 @@
 package consulo.markdown;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.AttributesFlyweightBuilder;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
-
 import jakarta.annotation.Nonnull;
+import org.intellij.plugins.markdown.highlighting.MarkdownHighlighterColors;
 
 /**
  * @author VISTALL
  * @since 28/04/2023
  */
 @ExtensionImpl
-public class LightAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider {
-  @Nonnull
-  @Override
-  public String getColorSchemeName() {
-    return EditorColorsScheme.DEFAULT_SCHEME_NAME;
-  }
+public class LightAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
+    @Override
+    public void extend(Builder builder) {
+        builder.add(MarkdownHighlighterColors.BOLD_ATTR_KEY, MarkdownHighlighterColors.TEXT_ATTR_KEY, AttributesFlyweightBuilder
+            .create()
+            .withBoldFont()
+            .build());
 
-  @Nonnull
-  @Override
-  public String getColorSchemeFile() {
-    return "/colorSchemes/MarkdownDefault.xml";
-  }
+      builder.add(MarkdownHighlighterColors.ITALIC_ATTR_KEY, MarkdownHighlighterColors.TEXT_ATTR_KEY, AttributesFlyweightBuilder
+          .create()
+          .withItalicFont()
+          .build());
+    }
+
+    @Nonnull
+    @Override
+    public String getColorSchemeId() {
+        return EditorColorsScheme.DEFAULT_SCHEME_NAME;
+    }
 }
